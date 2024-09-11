87c9c8107ba791061c9ed36e9da0d476c6c9a594
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2024-09-11 17:44:34 +0900

    マージ

--- Game/GameScene.cpp
+++ Game/GameScene.cpp
@@ -21,23 +21,14 @@ void GameScene::OnInitialize() {
 	sunLight_->direction = Vector3(0.0f, -1.0f, 0.0f).Normalized();
 	RenderManager::GetInstance()->SetSunLight(sunLight_);
 
-	blockParticles_ = std::make_shared<BlockParticles>();
-	blockParticles_->Initialize(player_.get());
-
-	bulletManager_ = std::make_shared<BulletManager>();
-	bulletManager_->Initialize();
-
-	blockManager_ = std::make_shared<BlockManager>();
-	blockManager_->Initialize(blockParticles_.get());
-
-	enemyManager_ = std::make_shared<EnemyManager>();
-	enemyManager_->Initialize();
-	enemyManager_->SetBulletManager(bulletManager_);
 	score_ = std::make_shared<Score>();
-
 	player_ = std::make_shared<Player>();
 	map_ = std::make_shared<Map>();
-	
+
+	blockParticles_ = std::make_shared<BlockParticles>();
+	blockParticles_->Initialize();
+
+
 	map_->SetPlayer(player_);
 	map_->SetBlockParticles(blockParticles_);
 	map_->SetScore(score_);
@@ -75,11 +66,6 @@ void GameScene::OnUpdate() {
 		map_->CheckCollision();
 		CollisionManager::GetInstance()->CheckCollision();
 		blockParticles_->Update();
-
-		// �N���A�`�F�b�N
-		if (score_->GetIsClear()) {
-			gameClearCamera_->SetCameraPosition(-(float(map_->GetMapRow()) + MapProperty::kBlockSize * 2.0f));
-			player_->SetGameClearPosPosition(gameClearCamera_->GetEndCameraPos().z + 80.0f);
 		// リセット
 		if (input->IsKeyTrigger(DIK_R)) {
 			player_->Reset();
@@ -87,8 +73,14 @@ void GameScene::OnUpdate() {
 			map_->Generate();
 			score_->Reset();
 		}
-	}
-	break;
+
+		// クリアしたか
+		if (score_->GetIsClear()) {
+			gameClearCamera_->SetCameraPosition(-(float(map_->GetMapRow()) + MapProperty::kBlockSize * 2.0f));
+			player_->SetGameClearPosPosition(gameClearCamera_->GetEndCameraPos().z + 80.0f);
+			GameProperty::state_ = GameProperty::kResult;
+		}
+		break;
 	case GameProperty::kResult:
 	{
 		// カメラが動いているとき
@@ -96,7 +88,7 @@ void GameScene::OnUpdate() {
 			gameClearCamera_->Update();
 		}
 		else {
-			// リセット
+			// クリアしたら
 			if (input->IsKeyTrigger(DIK_R)) {
 				player_->Reset();
 				followCamera_->Reset();
@@ -110,9 +102,7 @@ void GameScene::OnUpdate() {
 	break;
 	default:
 		break;
-		enemyManager_->Reset();
-		player_->Reset();
-		followCamera_->Reset();
+	}
 	}
 }
 
